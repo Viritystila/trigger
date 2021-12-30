087467a3b9d1118057577d8e777868a64dfa2732
Mikael Reponen <mikael.reponen@gmail.com>
2021-12-30 19:15:07 +0200

    changed defproject to org.viritystila/trigger

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject trigger "0.1.0-SNAPSHOT"
+(defproject org.viritystila/trigger "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/Viritystila/cutter"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :repositories [["jcenter" "https://jcenter.bintray.com/"]]
